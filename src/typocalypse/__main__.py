23c0bbd8cce865e989a7175bdfd9e34ad78e5afc
Nicolas van Kempen <nvankemp@gmail.com>
2024-11-28 00:17:41 -0500

    Act on file or directory recursively

--- src/typocalypse/__main__.py
+++ src/typocalypse/__main__.py
@@ -1,27 +1,33 @@
 import argparse
+import os
 
 from . import typocalypse
+
+
+def process_file(args: argparse.Namespace, path: str) -> None:
+    with open(path, "r") as file:
+        code = file.read()
+    code = typocalypse.transform(code, args.override_existing_annotation)
+    with open(path, "w") as file:
+        file.write(code)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--override-existing-annotation", action="store_true")
-    parser.add_argument("--annotate-all-variables", action="store_true")
+    parser.add_argument("path", type=str, help="path to a file or directory")
     args = parser.parse_args()
 
-    input_code = """
-class A:
-    def f(self, x):
-        pass
-"""
-    print("Original Code:")
-    print(input_code)
-    print("\nTransformed Code:")
-    print(typocalypse.transform(input_code))
+    if os.path.isfile(args.path):
+        process_file(args, args.path)
+    elif os.path.isdir(args.path):
+        for root, _, files in os.walk(args.path):
+            for file in files:
+                if file.endswith(".py"):
+                    process_file(args, os.path.join(root, file))
+    else:
+        raise ValueError(f"invalid path: {args.path}")
 
 
 if __name__ == "__main__":
     main()
-
-
-# Add Any to anything that's not a function call OR literal.
